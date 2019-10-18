1a2de5cb612c94ccd400bbb7c0f61eafc00639ab
abdelfattah <abdelfattahrasras@gmail.com>
2019-10-18 17:59:04 +0300

    update project

---
+++ springboottut/src/main/java/com/example/demo/SpringboottutApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboottutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboottutApplication.class, args);
+	}
+
+}
